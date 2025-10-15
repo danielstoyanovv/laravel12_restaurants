56f2a8c2f6bdd3d9b2fb5ab7f40a9be8ae3e3ab7
daniel <daniel.stoyanovv@gmail.com>
2025-10-15 10:58:17 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
+
 // Protected routes (token required)
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('restaurants', RestaurantController::class);
